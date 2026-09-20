8a289c7d0cecfd3bbf8848ef45f001ab4cad8c78
Nathan Tatsuta <natetats@gmail.com>
2026-09-19 17:57:41 -0700

    Add main datacenter env

--- pde_control_gym/src/environments3d/__init__.py
+++ pde_control_gym/src/environments3d/__init__.py
@@ -1,3 +1,3 @@
-from pde_control_gym.src.environments3d.base_env_3d import PDEEnv3D
+from pde_control_gym.src.environments3d.datacenter3d import Datacenter3D
 
-__all__ = ["PDEEnv3D"]
+__all__ = ["DataCenter3D"]
